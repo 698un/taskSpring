df9ede027f312f7a5c98e013f5eff145843a08e6
698un <ya698un@yandex.ru>
2022-02-16 19:13:28 +0200

    Initial commit

---
+++ src/main/java/by/unil2/itstep/taskSpring/TaskSpringApplication.java
@@ -0,0 +1,23 @@
+package by.unil2.itstep.taskSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ *
+ База данных: itstepforunil2
+ Username: userunil2
+ E-mail: ya698un@yandex.ru
+ */
+
+
+
+@SpringBootApplication
+public class TaskSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSpringApplication.class, args);
+	}
+
+	}
---
+++ src/test/java/by/unil2/itstep/taskSpring/TaskSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package by.unil2.itstep.taskSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
